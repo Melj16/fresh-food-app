e6801ff19c3f427ff69e2f1dfdfd18e2010d756c
Melissa Jiang <melissajiang3@gmail.com>
2023-04-20 19:54:29 -0400

    new addition

--- flask-app/src/matt/matt.py
+++ flask-app/src/matt/matt.py
@@ -23,3 +23,20 @@ def get_inventory(userID):
     the_response.mimetype = 'application/json'
     return the_response
 
+# Get the inventory of a specific user
+@matt.route('/inventory/<userID>', methods=['GET'])
+def get_inventory(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT first_name, food_name, expiration_date FROM Users \
+        inner join User_Food UF on Users.user_id = UF.user_id \
+        inner join Food F on UF.food_id = F.food_id \
+        WHERE UF.user_id = {0};'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
