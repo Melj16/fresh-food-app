ff3544e86cdba4a7efde3f91fc2383f2e7d624f8
ajg <ajg52401@gmail.com>
2023-04-20 21:47:02 -0400

    matt

--- flask-app/src/matt/matt.py
+++ flask-app/src/matt/matt.py
@@ -27,9 +27,95 @@ def get_inventory(userID):
 @matt.route('/inventory/<userID>', methods=['GET'])
 def get_inventory(userID):
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT first_name, food_name, expiration_date FROM Users \
-        outter join User_Food UF on Users.user_id = UF.user_id \
-        outter join Food F on UF.food_id = F.food_id \
+    cursor.execute('SELECT expiration_date FROM User_Food \
+        inner join Food F on F.food_id = UF.food_id \
+        WHERE UF.user_id = {0};'.format(userID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# create grocery lists 
+@matt.route('/createGroceryList', methods=['POST'])
+def create_grocery_list():
+    data = request.json
+    current_app.logger.info(data)
+
+    list_name = data['list_name']
+    user_id = data['user_id']
+    household_id = data['household_id']
+
+    query = 'insert into Grocery_List (list_name, user_id, household_id) values ("'
+    query += list_name + '", '
+    query += str(user_id) + ', '
+    query += str(household_id) + ')'
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success'
+
+# update a grocery list by adding a food
+@matt.route('/updateGroceryList', methods=['PUT'])
+def update_grocery_list():
+    data = request.json
+    current_app.logger.info(data)
+
+    list_id = data['list_id']
+    food_name = data['food_name']
+
+    query = 'insert into Food (list_id, food_name) values ('
+    query += str(list_id) + ', "'
+    query += food_name + '")'
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success'
+
+# delete a grocery list
+@matt.route('/deleteGroceryList/<listID>', methods=['DELETE'])
+def delete_grocery_list(listID):
+    cursor = db.get_db().cursor()
+    cursor.execute('DELETE FROM Grocery_List WHERE list_id = {0};'.format(listID))
+    db.get_db().commit()
+
+    the_response = "Successfully Deleted Grocery List {0}".format(listID)
+
+    return the_response
+
+# Get the foods from a grocery list
+@matt.route('/groceryList/<listID>', methods=['GET'])
+def get_grocery_list(listID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT food_name FROM Grocery_List \
+        inner join Food F on Grocery_List.list_id = F.list_id \
+        WHERE Grocery_List.list_id = {0};'.format(listID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get the price of a specific food
+@matt.route('/inventory/<userID>', methods=['GET'])
+def get_inventory(userID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT cost FROM Food \
+        inner join User_Food UF on UF.food_id = F.food_id \
         WHERE UF.user_id = {0};'.format(userID))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
